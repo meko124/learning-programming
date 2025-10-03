9a45b1e15f21ce15635d24c9a5f7cabbe5be173d
Muhammad Eko Kuswanto <mekokuswanto123@gmail.com>
2025-10-03 18:14:19 +0700

    add file java

---
+++ java/learn-java/src/com/basic/NumberTypeData.java
@@ -0,0 +1,14 @@
+package com.basic;
+
+public class NumberTypeData {
+	public static void main(String[] args) {
+		byte myByte = 100;
+		short myShort = 1000;
+		int myInt = 10000;
+		long myLong = 100000;
+		long myLongTwo = 100000L;
+		System.out.println(myByte);
+		System.out.println(myInt);
+		
+	}
+}

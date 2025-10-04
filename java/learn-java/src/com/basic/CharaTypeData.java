3f3dccb6aafe137737b4f649b4ae22536d8d563f
Muhammad Eko Kuswanto <mekokuswanto123@gmail.com>
2025-10-04 17:43:01 +0700

    add line code and java file

---
+++ java/learn-java/src/com/basic/CharaTypeData.java
@@ -0,0 +1,9 @@
+package com.basic;
+
+public class CharaTypeData {
+	
+	public static void main(String[] args) {
+		
+		System.out.print("hello boy");
+	}
+}
--- java/learn-java/src/com/basic/NumberTypeData.java
+++ java/learn-java/src/com/basic/NumberTypeData.java
@@ -1,13 +1,19 @@
 package com.basic;
 
 public class NumberTypeData {
+	
 	public static void main(String[] args) {
+		
 		byte myByte = 100;
 		short myShort = 1000;
 		int myInt = 10000;
 		long myLong = 100000;
 		long myLongTwo = 100000L;
+		float myFloat = 10.10f;
+		double myDouble = 10.10;
+		
 		System.out.println(myByte);
+		System.out.println(myDouble);
 		System.out.println(myInt);
 		
 	}

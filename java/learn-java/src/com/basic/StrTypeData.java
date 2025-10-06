3a3d7d7a366ec127a976e3e1dc0395a361bc3cd3
MUHAMMAD EKO KUSWANTO <mekokuswanto123@gmail.com>
2025-10-06 18:01:59 +0700

    add file java

---
+++ java/learn-java/src/com/basic/StrTypeData.java
@@ -0,0 +1,18 @@
+package com.basic;
+
+public class StrTypeData {
+	
+	public static void main(String[] args) {
+		
+		String firstName = "Muhammad Eko";
+		String lastName = "Kuswanto";
+		String fullName = firstName + " " + lastName;
+		int age;
+		age = 10;
+		
+		System.out.println(firstName);
+		System.out.println(lastName);
+		System.out.println(fullName);
+		System.out.println(age);
+	}
+}

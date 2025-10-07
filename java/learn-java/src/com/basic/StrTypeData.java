29c612606fcd779fbd270262950036c9ae8d750c
MUHAMMAD EKO KUSWANTO <mekokuswanto123@gmail.com>
2025-10-07 17:52:19 +0700

    add line code and add new file java

--- java/learn-java/src/com/basic/StrTypeData.java
+++ java/learn-java/src/com/basic/StrTypeData.java
@@ -9,10 +9,14 @@ public class StrTypeData {
 		String fullName = firstName + " " + lastName;
 		int age;
 		age = 10;
+		var onePlusone = "two";
+		final String window = "one plus one equel?";
 		
 		System.out.println(firstName);
 		System.out.println(lastName);
 		System.out.println(fullName);
 		System.out.println(age);
+		System.out.println(onePlusone);
+		System.out.println(window);
 	}
 }

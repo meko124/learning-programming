9e20d80a7fb77d840daf48d6be68e0128eccd47d
MUHAMMAD EKO KUSWANTO <mekokuswanto123@gmail.com>
2025-10-08 17:59:28 +0700

    add line code

--- java/learn-java/src/com/basic/NonPrimitiveTypeData.java
+++ java/learn-java/src/com/basic/NonPrimitiveTypeData.java
@@ -3,6 +3,11 @@ package com.basic;
 public class NonPrimitiveTypeData {
 	public static void main(String[] args) {
 		
-		System.out.println("hello");
+		Byte myByte = 25;
+		int myChange = 100;
+		Integer thisIsObject = myChange;
+		long myLong = thisIsObject.longValue();
+		System.out.println(myByte);
+		System.out.println(myLong);
 	}
 }

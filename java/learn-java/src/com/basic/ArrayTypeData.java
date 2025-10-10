6aaafb86855feafb77ee3628a01b1ab6f9ed9abc
MUHAMMAD EKO KUSWANTO <mekokuswanto123@gmail.com>
2025-10-10 17:55:02 +0700

    add line code

--- java/learn-java/src/com/basic/ArrayTypeData.java
+++ java/learn-java/src/com/basic/ArrayTypeData.java
@@ -1,4 +1,5 @@
 package com.basic;
+import java.util.Arrays;
 
 public class ArrayTypeData {
 	public static void main(String[] args) {
@@ -10,9 +11,20 @@ public class ArrayTypeData {
 		stringArray[1] = "Eko";
 		stringArray[2] = "Kuswanto";
 		
+	int[] arrayInt = new int[]{
+		1,2,4,5
+	};
+	
+	long[] arrayLong = {
+		10L,20L
+	};;
 		System.out.println(stringArray[0]);
 		System.out.println(stringArray[1]);
 		System.out.println(stringArray[2]);
+		System.out.println(Arrays.toString(arrayInt));
+		System.out.println(Arrays.toString(arrayLong));
+		
+		
 		
 		stringArray[1] = "Ana";
 		System.out.println(stringArray[1]);

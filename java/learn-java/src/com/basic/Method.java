74d90aeec8ed03656d84792c20d5ada1604450de
MUHAMMAD EKO KUSWANTO <mekokuswanto123@gmail.com>
2025-10-17 17:42:28 +0700

    add line code in java

--- java/learn-java/src/com/basic/Method.java
+++ java/learn-java/src/com/basic/Method.java
@@ -7,6 +7,9 @@ public class Method {
 		sayName("Muhammad Eko","Kuswanto");
 		int showTheIncrease = calculateTheIncrease(20,30);
 		System.out.println(showTheIncrease);
+		int[] values = {60,60,40};
+		calculateSubjectGrades("Eko",values);
+		calculateSubjectGrades("Siti",86,75,90);
 	}
 	static void sayHelloWorld() {
 		System.out.println("Hello 1");
@@ -17,7 +20,19 @@ public class Method {
 		System.out.println("hello " + firstName + " " + lastName);
 	}
 	static int calculateTheIncrease(int value1, int value2) {
-		var total = value1 + value2;
-		return total;
+		int sum = value1 + value2;
+		return sum;
+	}
+	static void calculateSubjectGrades(String name, int... values) {
+		int sum = 0;
+		for(int value : values) {
+			sum += value;
+		}
+		int finalValue = sum / values.length;
+		if(finalValue >= 65 ) {
+			System.out.println("your pased");
+		} else {
+			System.out.println("your failed");
+		}
 	}
 }

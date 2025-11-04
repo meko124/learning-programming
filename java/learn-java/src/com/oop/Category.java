3f76b901a9adc20c34d32bfddc651bf0c8415083
MUHAMMAD EKO KUSWANTO <mekokuswanto123@gmail.com>
2025-11-04 23:21:47 +0700

    add new file in java

---
+++ java/learn-java/src/com/oop/Category.java
@@ -0,0 +1,21 @@
+package com.oop;
+
+public class Category {
+	private String id;
+	private boolean expensive;
+	
+	public String getId() {
+		return this.id;
+	}
+	public void setId(String id) {
+		if(id != null) {
+			this.id = id;
+		}
+	}
+	public boolean isExpensive() {
+		return this.expensive;
+	}
+	public void setExpensive(boolean expensive) {
+		this.expensive = expensive;
+	}
+}
---
+++ java/learn-java/src/com/oop/CategoryApp.java
@@ -0,0 +1,9 @@
+package com.oop;
+
+public class CategoryApp {
+	public static void main(String[] args)  {
+		var category = new Category();
+		category.setId("Id");
+		System.out.println(category.getId());
+	}
+}

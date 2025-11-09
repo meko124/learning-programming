078d7aafecfee8107098ef153df9dbc48a8e622c
MUHAMMAD EKO KUSWANTO <mekokuswanto123@gmail.com>
2025-11-10 04:51:53 +0700

    make a new file in java

---
+++ java/learn-java/src/com/oop/Company.java
@@ -0,0 +1,22 @@
+package com.oop;
+
+public class Company {
+	
+	private String name;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name
+	}
+	
+	public class Employee {
+		private String name;
+		public String getName() {
+			return name;
+		}
+		public void setName(String name) {
+			this.name = name;
+		}
+	}
+}

5aa9ebcf09e102365d5313d6f4d32666a7e57893
MUHAMMAD EKO KUSWANTO <mekokuswanto123@gmail.com>
2025-11-20 21:55:28 +0700

    make a new file and add line code in java

---
+++ java/learn-java/src/com/oop/EnumApp.java
@@ -0,0 +1,17 @@
+package com.oop;
+
+public class EnumApp {
+	public static void main(String[] args) {
+		
+		Customer customer = new Customer();
+		customer.setName("Eko");
+		customer.setLevel(Level.VIP);
+		
+		System.out.println(customer.getName());
+		System.out.println(customer.getLevel());
+		System.out.println(customer.getLevel().getDescription());
+		
+		String levelName = Level.VIP.name();
+		System.out.println(levelName)
+	}
+}
--- java/learn-java/src/com/oop/Level.java
+++ java/learn-java/src/com/oop/Level.java
@@ -1,7 +1,15 @@
 package com.oop;
 
 public enum Level {
-	STANDARD,
-	PREMIUM,
-	VIP
+	STANDARD("Standart level"),
+	PREMIUM("Premium level"),
+	VIP("Vip level");
+	
+	private String description;
+	Level(String description) {
+		this.description = description;
+	}
+	public String getDescription() {
+		return description;
+	}
 }

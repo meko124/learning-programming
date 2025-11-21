68b52dda625857a0a0fbad2de9d6feb88ff44da3
MUHAMMAD EKO KUSWANTO <mekokuswanto123@gmail.com>
2025-11-22 03:48:08 +0700

    add line code in java

--- java/learn-java/src/com/oop/EnumApp.java
+++ java/learn-java/src/com/oop/EnumApp.java
@@ -12,6 +12,14 @@ public class EnumApp {
 		System.out.println(customer.getLevel().getDescription());
 		
 		String levelName = Level.VIP.name();
-		System.out.println(levelName)
+		System.out.println(levelName);
+		
+		Level level = Level.valueOf("PREMIUM");
+		System.out.println(level);
+		
+		System.out.println("Print Level");
+		for(var prints : Level.values()) {
+			System.out.println(prints);
+		}
 	}
 }

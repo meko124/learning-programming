5f4437d1db4bbca9f95fd9afedd1c5235bcb4e7f
MUHAMMAD EKO KUSWANTO <mekokuswanto123@gmail.com>
2025-12-06 21:19:22 +0700

    add line code and make a new file in java

---
+++ java/learn-java/runStringBuilderApp.bat
@@ -0,0 +1,6 @@
+@echo off
+echo Compiling all Java files...
+javac -d out src\com\classes\*.java
+echo.
+echo Running program...
+java -cp out com.classes.StringBuilderApp
--- java/learn-java/src/com/classes/StringApp.java
+++ java/learn-java/src/com/classes/StringApp.java
@@ -6,9 +6,20 @@ public class StringApp {
 		String fullName = "Muhammad Eko Kuswanto";
 		String fullNameLowerCase = fullName.toLowerCase();
 		String fullNameUpperCase = fullName.toUpperCase();
+		String passwordBad = "		secret124	";
+		String passwordClean = passwordBad.trim();
+		boolean isEko = fullName.contains("Eko");
 		
 		System.out.println(fullName);
 		System.out.println(fullNameLowerCase);
 		System.out.println(fullNameUpperCase);
+		System.out.println(passwordClean);
+		System.out.println(isEko);
+		
+		
+		String[] names = fullName.split(" ");
+		for(var value : names) {
+			System.out.println(value);
+		}
 	}
 }
